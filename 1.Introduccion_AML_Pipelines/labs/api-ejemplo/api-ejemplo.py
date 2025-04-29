e76fbf0091924b0980d8cf42669e8c866c7bcd3a
ricardo <ricardo@enmotionvalue.com>
2025-04-29 10:13:11 +0000

    update

---
+++ 1.Introduccion_AML_Pipelines/labs/api-ejemplo/api-ejemplo.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+
+# Crear una instancia de Flask
+app = Flask(__name__)
+# Definir una ruta básica
+@app.route('/')
+def home():
+    return "¡Bienvenido a mi aplicación Flask!"
+# Definir una ruta para una API RESTful
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    # Datos de ejemplo
+    data = {
+        "nombre": "Flask",
+        "descripcion": "Un microframework de Python para desarrollo web",
+        "version": "2.3.2"
+    }
+    # Devolver los datos en formato JSON
+    return jsonify(data)
+# Ejecutar la aplicación
+if __name__ == '__main__':
+    app.run(debug=True)
