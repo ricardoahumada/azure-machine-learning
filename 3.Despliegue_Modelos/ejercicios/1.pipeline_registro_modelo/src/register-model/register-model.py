5f96fb5eaafd77c390bf9a8743eb9ee5b04e0aac
ricardoag <ricardo@enmotionvalue.com>
2025-05-25 15:52:30 +0200

    azure ml 3

---
+++ 3.Despliegue_Modelos/ejercicios/1.pipeline_registro_modelo/src/register-model/register-model.py
@@ -0,0 +1,66 @@
+
+import pandas as pd
+import numpy as np
+import json
+
+import argparse
+from pathlib import Path
+
+import mlflow
+
+from azure.ai.ml import MLClient
+from azure.ai.ml.entities import Model
+from azure.identity import DefaultAzureCredential
+
+
+def main(args):
+    mlflow.autolog()
+
+    metrics = read_metrics(args.metrics_input)
+    register_model(args.model_input, metrics, args.model_name)
+
+
+def read_metrics(data_path):
+    # Leer las métricas desde el archivo
+    with open(data_path, "r") as f:
+        metrics = json.load(f)
+
+    print(f"Métricas leídas:", metrics)
+
+    return metrics
+
+
+def register_model(model_path, metrics, model_name):
+    # Inicializar el cliente de Azure ML
+    ml_client = MLClient.from_config(credential=DefaultAzureCredential())
+
+    mse = metrics['mse']
+
+    # Registrar el modelo
+    registered_model = ml_client.models.create_or_update(
+        Model(
+            path=model_path,
+            name=model_name,
+            description=f"Modelo registrado con accuracy={mse}"
+        )
+    )
+
+    print(f"Modelo {model_name} registrado con éxito.") 
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_input", dest='model_input', type=str, required=True)
+    parser.add_argument("--metrics_input", dest='metrics_input', type=str, required=True)
+    parser.add_argument("--model_name", dest='model_name', type=str, required=True)
+    args = parser.parse_args()
+
+    return args
+
+
+# run script
+if __name__ == "__main__":
+    args = parse_args()
+    print('args:', args)
+    main(args)
---
+++ 3.Despliegue_Modelos/ejercicios/1.pipeline_registro_modelo/src/train-rf-model/train-rf-model.py
@@ -0,0 +1,91 @@
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+import argparse
+from pathlib import Path
+
+import mlflow
+import glob
+
+
+def main(args):
+    mlflow.autolog()
+
+    df = get_data(args.input_data)
+    model, X_test, y_test = train_model('price', df, args.n_estimators, 0.3)
+
+    mlflow.sklearn.save_model(model, args.model_output)
+
+    X_test.to_csv((Path(args.output_data) / "housing_X_test.csv"), index = False)
+    y_test.to_csv((Path(args.output_data) / "housing_y_test.csv"), index = False)
+
+    
+
+def get_data(data_path):
+
+    all_files = glob.glob(data_path + "/*.csv")
+    df = pd.concat((pd.read_csv(f) for f in all_files), sort=False)
+    
+    return df
+
+
+def train_model(target, data, n_estimators, test_size=0.2):
+    # 4. Separar características (X) y objetivo (y)
+    X = data.drop(columns=[target])  # Todas las columnas excepto target
+    y = data[target]  # Variable objetivo
+
+    # 5. Dividir los datos en entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=42)
+
+    # 6. Preprocesamiento de datos
+    # Identificar columnas numéricas y categóricas
+    numeric_features = X.select_dtypes(include=['int64', 'float64']).columns
+    categorical_features = X.select_dtypes(include=['object']).columns
+
+    # Crear un preprocesador con StandardScaler para numéricas y OneHotEncoder para categóricas
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', StandardScaler(), numeric_features),
+            ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_features)
+        ]
+    )
+
+    # 7. Crear un pipeline con el preprocesador y el modelo rf
+    mlflow.log_param("n_estimators:", n_estimators)
+
+    rf_model = Pipeline(steps=[
+        ('preprocessor', preprocessor),
+        # n_estimators es el parámetro de regularización
+        ('regressor', RandomForestRegressor(n_estimators=n_estimators, random_state=42))
+    ])
+
+    # 8. Entrenar el modelo
+    rf_model.fit(X_train, y_train)
+
+    return rf_model, X_test, y_test
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_data", dest='input_data', type=str)
+    parser.add_argument("--n_estimators", dest='n_estimators', type=int, default=0.01)
+    
+    parser.add_argument("--model_output", dest='model_output',type=str)
+    parser.add_argument("--output_data", dest='output_data',type=str)
+    args = parser.parse_args()
+
+    return args
+
+
+# run script
+if __name__ == "__main__":
+    args = parse_args()
+    print('args:', args)
+    main(args)
